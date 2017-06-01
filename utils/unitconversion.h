54394e39a8d6c4dda89c912a18321896a90e2869
Jason <idleuncle@gmail.com>
2017-06-01 23:01:17 +0800

    Add pixel_to_...() unit conversion functions.

--- utils/unitconversion.h
+++ utils/unitconversion.h
@@ -4,14 +4,20 @@
 namespace utils{
 
     double mm_to_inch(double mm){return mm / 25.4;};
-    double inch_to_mm(double inch){return inch * 25.4;};
     double mm_to_point(double mm){return mm * 72 / 25.4;};
+    double mm_to_pixel(double mm, double dpi){return dpi * mm / 25.4;};
+
+    double inch_to_mm(double inch){return inch * 25.4;};
     double inch_to_point(double inch){return inch * 72;};
+    double inch_to_pixel(double inch, double dpi){return dpi * inch;};
+
     double point_to_mm(double point){return point * 25.4 / 72;};
     double point_to_inch(double point){return point / 72;};
-    double mm_to_pixel(double mm, double dpi){return dpi * mm / 25.4;};
-    double inch_to_pixel(double inch, double dpi){return dpi * inch;};
     double point_to_pixel(double point, double dpi){return dpi * point / 72;};
+
+    double pixel_to_mm(double pixel, double dpi){return pixel * 25.4 / dpi;};
+    double pixel_to_inch(double pixel, double dpi){reutrn pixel / dpi;};
+    double pixel_to_point(double pixel, dobule dpi){return pixel * 72 / dpi;};
 
 } // namespace utils
 
