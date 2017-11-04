9b4434c5113cac517ecf1886cf2b58a7140ea1e1
Jason <idleuncle@gmail.com>
2017-11-04 12:10:57 +0800

    pdf2htmlEX-config.h

---
+++ tools/ofd2html/pdf2htmlEX-config.h
@@ -0,0 +1,24 @@
+/*
+ * config.h
+ * Compile time constants
+ *
+ * Copyright (C) 2012-2014 Lu Wang <coolwanglu@gmail.com>
+ */
+
+
+#ifndef PDF2HTMLEX_CONFIG_H__
+#define PDF2HTMLEX_CONFIG_H__
+
+#include <string>
+
+#define ENABLE_SVG 1
+
+namespace pdf2htmlEX {
+
+static const std::string PDF2HTMLEX_VERSION = "";
+static const std::string PDF2HTMLEX_PREFIX = "/usr/local";
+static const std::string PDF2HTMLEX_DATA_PATH = "/usr/local""/share/pdf2htmlEX";
+
+} // namespace pdf2htmlEX
+
+#endif //PDF2HTMLEX_CONFIG_H__
